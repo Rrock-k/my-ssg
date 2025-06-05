566d5e00f10045cdcdc23755d8e0a481454a73db
Rrock-k <32955276+Rrock-k@users.noreply.github.com>
2025-06-05 14:48:23 +0700

    feat: support per-page static props

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,21 +1,23 @@
-import { Routes, Route } from "react-router-dom"
-import HomePage from "../pages/index"
-import AboutPage from "../pages/about"
-import ContactPage from "../pages/contact"
-import Layout from "../components/Layout"
-import TodoPage from "../pages/todo"
-import ThemePage from "../pages/theme"
+import { Routes, Route } from "react-router-dom";
+import HomePage from "../pages/index";
+import AboutPage from "../pages/about";
+import ContactPage from "../pages/contact";
+import Layout from "../components/Layout";
+import TodoPage from "../pages/todo";
+import ThemePage from "../pages/theme";
 
-export default function AppRoutes() {
+export type PagePropsMap = Record<string, any>;
+
+export default function AppRoutes({ pageProps = {} }: { pageProps?: PagePropsMap } = {}) {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about.html" element={<AboutPage />} />
-        <Route path="/contact.html" element={<ContactPage />} />
-        <Route path="/todo.html" element={<TodoPage />} />
-        <Route path="/theme.html" element={<ThemePage />} />
+        <Route path="/" element={<HomePage {...pageProps["/"]} />} />
+        <Route path="/about.html" element={<AboutPage {...pageProps["/about.html"]} />} />
+        <Route path="/contact.html" element={<ContactPage {...pageProps["/contact.html"]} />} />
+        <Route path="/todo.html" element={<TodoPage {...pageProps["/todo.html"]} />} />
+        <Route path="/theme.html" element={<ThemePage {...pageProps["/theme.html"]} />} />
       </Route>
     </Routes>
-  )
+  );
 }
